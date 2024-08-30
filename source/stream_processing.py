8f90a8d3889f8e85d946122915a7a33824a59f3f
kylegraupe <91435626+kylegraupe@users.noreply.github.com>
2024-08-30 16:34:23 -0400

    Update stream_processing.py
    Reduced threads from 8 to 4. (Mac M1 Pro)

--- source/stream_processing.py
+++ source/stream_processing.py
@@ -94,7 +94,7 @@ def livestream_2(url):
         ffmpeg
         .input(url, **{'an': None, 'r': f'{FPS}'})  # Audio Disabled in second parameter.
         .output('pipe:', format='rawvideo', pix_fmt='bgr24')
-        .global_args('-threads', '8')
+        .global_args('-threads', '4')
         .global_args('-c:v', 'h264_videotoolbox')  # Use VideoToolbox for encoding
         .run_async(pipe_stdout=True, pipe_stderr=True)
     )
