6f1825deeafb90376c67bfcba6f46731c9a8dee1
kylegraupe <91435626+kylegraupe@users.noreply.github.com>
2024-09-02 10:40:55 -0400

    Terminal Logs & Versioning

--- source/main.py
+++ source/main.py
@@ -1,7 +1,12 @@
 import stream_processing
 import settings
+import time
 
 if __name__ == "__main__":
-    print('Hello, World!')
+    print(f'Application started at time {time.ctime()}')
+    print(f'\tVersion: {settings.VERSION}')
+    print(f'\tEnvironment: {settings.ENVIRONMENT}')
+    print(f'\tRTMP URL: {settings.RTMP_URL}')
+    print(f'\tListening Port: {settings.LISTENING_PORT}')
 
     stream_processing.livestream_2(settings.RTMP_URL)
--- source/settings.py
+++ source/settings.py
@@ -1,9 +1,11 @@
 # Application Environment
+VERSION = '1.0.0'
 ENVIRONMENT = 'development'
 TRAIN = False
 
 # RTMP/NGINX settings
-RTMP_URL='rtmp://10.0.0.17:1935/live/'
+# RTMP_URL='rtmp://10.0.0.17:1935/live/'
+RTMP_URL='rtmp://192.168.1.89:1935/live/'
 LISTENING_PORT=1935
 
 # DJI Mini 4 Pro Specs
